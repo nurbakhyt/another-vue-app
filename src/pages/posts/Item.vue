e4a4ac92420b95f7b6e9968ba31dc8b0e7fdbf19
Nurbakhyt <nurbakhyt@gmail.com>
2019-04-03 07:06:41 +0600

    added: Post page

--- src/pages/posts/Item.vue
+++ src/pages/posts/Item.vue
@@ -1,15 +1,41 @@
 <template>
-  <section>
-    Post #{{ $route.params.id }}
+  <section class="section post-page">
+
+    <post
+      v-if="!!post"
+      :post="post"
+      full
+    />
+
+    <h3 v-else>404. Not Found</h3>
+
   </section>
 </template>
 
 <script>
+  import {mapGetters} from 'vuex';
+  import Post from '../../components/Post';
+
   export default {
     name: 'PostItem',
+    components: {Post},
+    computed: {
+      ...mapGetters([
+        'users',
+        'posts'
+      ]),
+      post () {
+        let post = this.posts.find(item => item.id == this.$route.params.id);
+        if (!!post) {
+          return post;
+        }
+      }
+    }
   }
 </script>
 
 <style lang="scss">
+  .post-page {
 
+  }
 </style>
