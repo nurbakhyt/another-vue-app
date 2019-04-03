c9bec014641f355e1ee022b606d98b9d2449935a
Nurbakhyt <nurbakhyt@gmail.com>
2019-04-03 07:26:33 +0600

    feature: back btn

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,20 @@
 <template>
   <header class="header">
-    MyApp
+    <div>
+      <a
+        v-if="$route.name !== 'main-page'"
+        class="back-btn"
+        @click="$router.back(-1)"
+      >
+        < back
+      </a>
+    </div>
+
+    <div class="app-name">
+      MyApp
+    </div>
+
+    <div></div>
   </header>
 </template>
 
@@ -13,7 +27,14 @@
 <style lang="scss">
   .header {
     display: flex;
-    justify-content: center;
-    padding: 16px 24px;
+    justify-content: space-between;
+    padding-top: 16px;
+    padding-bottom: 16px;
+    position: relative;
+  }
+
+  .back-btn {
+    color: #3951a7;
+    cursor: pointer;
   }
 </style>
